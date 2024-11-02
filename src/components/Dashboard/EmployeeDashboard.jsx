be487b82e8327f81f07757daecd7378b8d49ec74
Saksham Verma <sakshamverma1000@gmail.com>
2024-11-02 11:58:35 +0530

    Third Commit

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../others/Header";
 import TaskTracker from "../others/TaskTracker";
+import TaskList from "../Tasks/TaskList";
 
 const EmployeeDashboard = () => {
   return (
     <div className="p-10 bg-[#1C1C1C] h-screen w-screen">
       <Header />
       <TaskTracker />
+      <TaskList />
     </div>
   );
 };
---
+++ src/components/Tasks/TaskList.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="w-full py-5  overflow-x-auto flex gap-5 flex-nowrap items-center justify-start h-[55%]  mt-10"
+    >
+      <div className=" flex-shrink-0 bg-red-400 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+      <div className=" flex-shrink-0 bg-indigo-600 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+      <div className=" flex-shrink-0 bg-blue-400 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+      <div className=" flex-shrink-0 bg-emerald-600 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+      <div className=" flex-shrink-0 bg-orange-400 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+      <div className=" flex-shrink-0 bg-teal-600 w-[400px] py-5 px-3 h-full rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-700 text-sm rounded-lg px-3 py-1">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+
+        <h2 className="mt-5 font-semibold text-2xl">
+          Learn Backend Development
+        </h2>
+        <p className="text-sm mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          eligendi?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;

603989c0ff5b7ca73cb0994f1a342fc79fd45d01
Saksham Verma <sakshamverma1000@gmail.com>
2024-12-16 11:16:04 +0530

    Fixed the layout of Login page, now hvae to make it responsnive, and also improve UI of login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -16,68 +16,62 @@ const Login = ({ handleLogin }) => {
   return (
     <>
       <div className=" flex items-center justify-between h-screen ">
-        {/* DIV 1*/}
-        <div id="login-div1">
-          <motion.div
-            id="gola1"
-            animate={{
-              x: [0, 150, 0],
-              y: [0, -250, 0],
-              skewX: -20,
-            }}
-            transition={{
-              duration: 3,
-              delay: 1,
-              ease: "easeInOut",
-            }}
-          ></motion.div>
+        {/* login-DIV 2*/}
+        <div className="login-div2">
+          <div className=" h-2/3 mt-20 border-3 rounded-xl border-emerald-600 py-5 bg-slate-200  ">
+            {/* FORM */}
+            <form
+              onSubmit={(e) => {
+                submitHandler(e);
+              }}
+              className="flex flex-col  "
+            >
+              <input
+                value={email}
+                autoComplete="on"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className=" responsive-input placeholder:text-gray-400 bg-transparent outline-none textx-xl border-2 border-emerald-600 rounded-full py-3 px-4 "
+                required
+                type="email"
+                placeholder="Enter your Email"
+              />
+              <input
+                value={password}
+                autoComplete="on"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className=" responsive-input placeholder:text-gray-400 bg-transparent outline-none textx-xl border-2 border-emerald-600 rounded-full py-3 px-4 mt-4  w-79"
+                required
+                type="password"
+                placeholder="Enter Password"
+              />
+              <motion.button
+                whileHover={{ scale: 1.112 }}
+                whileTap={{ scale: 0.9 }}
+                className="  border-none text-white outline-none textx-xl border-2 bg-emerald-600 rounded-full px-4 py-3 mt-4  w-79"
+              >
+                Log in
+              </motion.button>
+            </form>
+          </div>
+        </div>
 
+        {/* DIV 1*/}
+
+        <div id="login-div1">
           <h1 id="titleText">
             <span>
               WELCOME TO <br />
-              EMPLOYEE MANAGEMENT
+              EMPLOYEE
+              <br /> MANAGEMENT
               <br /> SITE
             </span>
           </h1>
-
-          <motion.div id="gola2"></motion.div>
         </div>
-
-        {/* DIV 2*/}
-        <div className=" responsive-login border-3 rounded-xl border-emerald-600 p-20 ">
-          <form
-            onSubmit={(e) => {
-              submitHandler(e);
-            }}
-            className="flex flex-col "
-          >
-            <input
-              value={email}
-              autoComplete="on"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              className=" responsive-input placeholder:text-gray-400 bg-transparent outline-none textx-xl border-2 border-emerald-600 rounded-full py-3 px-4 w-79"
-              required
-              type="email"
-              placeholder="Enter your Email"
-            />
-            <input
-              value={password}
-              autoComplete="on"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              className=" responsive-input placeholder:text-gray-400 bg-transparent outline-none textx-xl border-2 border-emerald-600 rounded-full py-3 px-4 mt-4  w-79"
-              required
-              type="password"
-              placeholder="Enter Password"
-            />
-            <button className="  border-none text-white outline-none textx-xl border-2 bg-emerald-600 rounded-full px-4 py-3 mt-4  w-79">
-              Log in
-            </button>
-          </form>
-        </div>
+        <motion.div id="gola1"></motion.div>
       </div>
     </>
   );
